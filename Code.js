6e7e0becbb9791c2202ef5dc0d416347114a44f7
Marhic Jerome <marhic.jerome@gmail.com>
2018-06-06 15:07:05 +0200

    Say Hello when clicking on menu button

---
+++ Code.js
@@ -0,0 +1,32 @@
+function onInstall(e) {
+  onOpen(e);
+}
+
+function onOpen(e) {
+  var menu = SpreadsheetApp.getUi().createAddonMenu();
+
+  menu.addItem('Say Hello', 'sayHello');
+  menu.addToUi();
+}
+
+function getActiveSheet() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  return ss.getActiveSheet();
+}
+
+// Displays data in the spreadsheet
+// @param sheet [Sheet] The current sheet
+// @param rows [Array] An array of rows, each row an array of String (one string per column)
+function setSheetData(sheet, rows) {
+ // The size of the range must match the size of the data.
+ var numRows = rows.length;
+ var numColumns = rows[0].length;
+ var range = sheet.getRange(1, 1, numRows, numColumns);
+ range.setValues(rows);
+}
+
+function sayHello() {
+  var sheet = getActiveSheet();
+  var rows = [["Hello"]];
+  setSheetData(sheet, rows);
+}
